61a3fc36a473a49aa34c6e8a23e72842184010e3
manga_osyo <manga.osyo@gmail.com>
2022-07-15 18:10:22 +0900

    補足

---
+++ 04/03_01/arrayX.rb
@@ -0,0 +1,9 @@
+animals = ["dog", "cat", "mouse"]
+# 末尾に追加
+animals.push("penguin")
+p animals
+# => ["dog", "cat", "mouse", "penguin"]
+# 先頭に追加
+animals.unshift("bird")
+p animals
+# => ["bird", "dog", "cat", "mouse", "penguin"]
