cb59e64eb90dbfc086033cfc05276ee2deed32d0
manga_osyo <manga.osyo@gmail.com>
2022-10-14 18:49:08 +0900

    第13回

---
+++ 13/07_05/medical_examinator1.rb
@@ -0,0 +1,13 @@
+class MedicalExaminator
+  def height=(height)
+    @height = height
+  end
+
+  def height
+    @height
+  end
+end
+
+examinator = MedicalExaminator.new
+examinator.height = 170
+p examinator.height
